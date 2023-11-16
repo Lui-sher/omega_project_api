d4e5fe80f8a799ff521924a9e4ac2c18941e8f56
luismarteloh <luis.martelohernandez@gmail.com>
2023-11-16 13:29:37 -0500

    creadas las rutas About, 404 in index

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
-	res.writeHead(200, {'Content-Type':'text/html'})
-	res.end('<h1>Hola Node.js</h1>')
+	if (req === "/"){
+		res.writeHead(200, {'Content-Type':'text/html'})
+		res.end('<h1>Welcome to Home</h1>')
+	}
+	if (req === "/about"){
+		res.writeHead(200, {'Content-Type':'text/html'})
+		res.end('<h1>This Is About</h1>')
+	} else {
+		res.writeHead(200, {'Content-Type':'text/html'})
+		res.end('<h1>404 Not Found Page</h1>')
+	}
+
 })
 
 const PORT = process.env.PORT || 5000
