a5c3f347499906e0aa29623829709af7eb2d242c
luismarteloh <luis.martelohernandez@gmail.com>
2023-11-16 10:10:12 -0500

    Frirs commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const http = require("http");
+const server = http.createServer((req, res) => {
+	res.writeHead(200, {'Content-Type':'text/html'})
+	res.end('<h1>Hola Node.js</h1>')
+})
+
+const PORT = process.env.PORT || 5000
+
+server.listen(PORT, () => console.log("Server running on", PORT === 5000 ? "http://localhost:5000" : PORT))
